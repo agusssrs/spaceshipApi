b7ef3c1cc91a83c94956028d4aabfca9d31bd698
agusssrs <agustinmancini2@hotmail.com>
2023-09-26 17:46:06 -0300

    api

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+require('dotenv').config();
+console.log(process.env.PORT);
+//# sourceMappingURL=index.js.map
---
+++ models/server.ts
@@ -0,0 +1,13 @@
+import express, {Express} from 'express';
+
+export class Server {
+    app: Express
+
+    constructor(){
+        this.app = express();
+    }
+
+    listen():void{
+
+    }
+}
